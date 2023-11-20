97c2b9ea79dfeb92f2dcecd970038348fbf56673
Abhishek Anil <141896050+Abhishekanil007@users.noreply.github.com>
2023-11-20 13:12:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const extendHex = (shortHex) => {
   // write your code here
+	const hexWithoutHash = shortHex.startsWith('#') ? shortHex.slice(1) : shortHex ;
+
+	const extendedHex = '#' + hexWithoutHash.split('').map(x => x + x).join('');
+	return extendedHex;
+	
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
+const shortHex = prompt("Enter Short Hex."); 
 alert(extendHex(shortHex));
